49f9f84a469122e4a5a6e03f1f464f88b609c8c7
Judgy53 <alexandre.gaubert94@orange.fr>
2024-07-12 12:50:29 +0200

    validate_utf8: Off by 1 and edge cases

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -4,24 +4,32 @@
 
 
 def validUTF8(data):
-    """ Determines if a given data set 
+    """ Determines if a given data set
         represents a valid UTF-8 encoding.
     """
+    # Number of bytes left to process in the current char
     char_byte_left = 0
     for entry in data:
-        entryByte = entry & 0b11111111
+        # Extract least significant byte from number
+        entryByte = entry % 0b11111111
 
         if char_byte_left == 0:
+            # Count leading 1's to know how many bytes in character
             shift = 7
             while entryByte >> shift & 1:
                 char_byte_left += 1
                 shift -= 1
-            if char_byte_left > 4:
+
+            # Continue if single byte char to avoid decrementing
+            if char_byte_left == 0:
+                continue
+
+            # Detect invalid byte count
+            if char_byte_left == 1 or char_byte_left > 4:
                 return False
-        else:
-            continue_identifier = entryByte >> 6
-            if continue_identifier != 0b10:
-                return False
-            char_byte_left -= 1
+        elif entryByte >> 6 != 0b10:
+            return False
+
+        char_byte_left -= 1
 
     return char_byte_left == 0
