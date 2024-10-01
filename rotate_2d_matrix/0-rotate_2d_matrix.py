d31fc843dc61c42a82dbd578ba7351a514500622
Judgy53 <9182@holbertonstudents.com>
2024-10-01 14:42:37 +0200

    rotate_2d_matrix: pycodestyle compliance

--- rotate_2d_matrix/0-rotate_2d_matrix.py
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module rotate_2d_matrix"""
 
+
 def rotate_2d_matrix(matrix):
     """Rotate a 2d matrix 90 degree clockwise"""
     matrix[:] = [list(row) for row in zip(*matrix[::-1])]
